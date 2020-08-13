23800f5d387c0adfb57c0cd9a5c9ba08db1b9277
Rio Waller <riowaller@gmail.com>
2020-08-13 09:32:01 -0700

    minor changes to prime_oi.py code

--- w2/prime_io.py
+++ w2/prime_io.py
@@ -1,22 +1,22 @@
 # Rio's Code for Week 2 LT
 
-# set run to Y 
+# set run to Y
 run = 'Y'
-# while run equals to Y run the code 
+# while run equals to Y run the code
 while run == 'Y':
     #ask user for starting number - convert input to integer
     print('Hello, please provide me a starting number  - integers only')
     start = int(input())
-    
+
     #ask user for ending number - convert input to integer
     print('and provide me a ending number and I will output the prime numbers between')
     end = int(input())
-    
+
     #if start is less than end give the user feedback, otherwise run code inside if statement
     if(start < end):
 
         #https://www.geeksforgeeks.org/python-program-to-print-all-prime-numbers-in-an-interval/
-        
+
         #I did some minor modification
         #I will cover this part in the video
         for val in range(start, end + 1):
@@ -29,15 +29,15 @@ while run == 'Y':
                         if n == val//2 + 1:
                             print(str(val) + ': is a prime number')
     else:
-        print('Starting number needs to be greater than ending number')    
-    
+        print('Starting number needs to be greater than ending number')
+
     #ask user if they run to run the code again
     print('Run again? Y for yes')
     run = input()
     #because run needs to be string, we don't need to convert
-    
+
     if(run != 'Y'):
 
         #if user enter anything but capital Y end the program
         print('Thanks for using my program!')
-    continue            
+    continue
