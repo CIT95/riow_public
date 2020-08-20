8bebfa6940e14d7c9b7b6f09c8fcaaca414dc6bb
Rio Waller <riowaller@gmail.com>
2020-08-20 11:53:17 -0700

    adding argument parameter file

--- w3/arg_params.py
+++ w3/arg_params.py
@@ -1,4 +1,43 @@
-def getUserInput(message):
+def timeToCharge(kWh, percentFull):
+    return kWh * (1 - percentFull)
+
+def determineNextSteps(time):
+    if(time > 12):
+        print('need more kWh')
+    else:
+        print('that will work') 
+
+chargeTime = timeToCharge(200, .80)
+print(chargeTime)
+determineNextSteps(chargeTime)
+
+chargeTime = timeToCharge(10, .50)
+print(chargeTime)
+determineNextSteps(chargeTime)
+
+
+
+
+""" def checkInput(user, message):
+    if(user == '' or message == ''):
+        greeting()
+    else:
+        greeting(user, message)    
+    
+
+def greeting(user='Who Dis?', message='No Message?' ): 
+    print('Hello ' + user + 'nice to hear you say ' + message)
+    
+
+user = input('Username') 
+message = input('Message') 
+
+result = checkInput(user, message)
+ """
+
+
+
+""" def getUserInput(message):
     return(input(message))
 
 def greeting(user='Who Dis?', age=3):
@@ -10,3 +49,4 @@ age = getUserInput('Your Age? --->')
 greeting()
 greeting(user, age)
 greeting('Rio')
+greeting(age = 9) """
---
+++ w3/default_params.py
@@ -0,0 +1,10 @@
+def addTwo(val=0):
+    return(val + 2)
+
+print(addTwo(4))
+print(addTwo())
+
+result = addTwo(10)
+
+
+
